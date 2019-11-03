0c13e614cf87d98a4cca07a20994f98e7503abbf
Amit <sharma88.amit@gmail.com>
2019-11-03 17:44:18 +0530

    task done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ export default class CreateComponent extends Component{
     render(){
         return (
             <div>
-                <h3>Welcome To Home Amit sharma</h3>
+                <h3>Welcome To Home Amit sharma bn</h3>
             </div>
         )
     }
